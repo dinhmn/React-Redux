88d5d0ff6393a9e8e3fdc901c872ed4f99516c14
Mai Ngọc Định <85937956+ngocdinh2k1@users.noreply.github.com>
2022-07-10 22:44:11 +0700

    Redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux/es/exports";
 import "./App.css";
 import Counter from "./components/Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const count = useSelector((state) => state.counter.count);
   return (
     <div className="App">
       <h2>The count from App is:{count} </h2>
-      <Counter count={count} setCount={setCount}></Counter>
+      <Counter></Counter>
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
-
-const Counter = (props) => {
-  const { count, setCount } = props;
-  const increment = () => {
-    setCount(count + 1);
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementbyvalue } from "../redux/counter";
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const handleIncrement = () => {
+    dispatch(increment());
   };
-  const decrement = () => {
-    setCount(count - 1);
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+  const handleIncrementByTen = () => {
+    dispatch(incrementbyvalue(10));
   };
   return (
     <div className="flex flex-col items-center p-5 bg-slate-700 text-white w-[400px] mx-auto mt-10 shadow">
       <h2 className="mb-5"> Count: {count}</h2>
-      <div className="flex justify-center items-center gap-x-5">
-        <button className="inline-block p-2" onClick={increment}>
+      <div className="flex items-center justify-center gap-x-5">
+        <button className="inline-block p-2" onClick={handleIncrement}>
           Increment
         </button>
-        <button className="inline-block p-2" onClick={decrement}>
+        <button className="inline-block p-2" onClick={handleDecrement}>
           Decrement
+        </button>
+        <button className="inline-block p-2" onClick={handleIncrementByTen}>
+          Increase by 10
         </button>
       </div>
     </div>
