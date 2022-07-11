38adf55da62d5183dd3cb7312b31f2895751ae0d
Mai Ngọc Định <85937956+ngocdinh2k1@users.noreply.github.com>
2022-07-11 23:03:19 +0700

    use Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux/es/exports";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux/es/exports";
 import "./App.css";
 import Counter from "./components/Counter";
+import useDarkMode from "./hooks/useDarkMode";
+import { toggleDarkMode } from "./redux-toolkit/globalSlice";
+import Card from "./uis/Card";
 
 function App() {
-  const count = useSelector((state) => state.counter.count);
+  // const count = useSelector((state) => state.counter.count);
+  // const globalOptions = useSelector((state) => state.global);
+  const dispatch = useDispatch();
+  // const handleToggleDarkmode = () => {
+  //   dispatch(toggleDarkMode(true));
+  // };
+  const [darkMode, setDarkMode] = useDarkMode();
+  // useEffect(() => {
+  //   dispatch(toggleDarkMode(darkMode));
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+  const handleToggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    dispatch(toggleDarkMode(!darkMode));
+  };
   return (
     <div className="App">
-      <h2>The count from App is:{count} </h2>
-      <Counter></Counter>
+      {/* <h2>The count from App is:{count} </h2>
+      <Counter></Counter> */}
+      <Card></Card>
+      <button onClick={handleToggleDarkMode}>Toggle dark mode</button>
     </div>
   );
 }
--- src/redux-toolkit/counterSlice.js
+++ src/redux-toolkit/counterSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
+import { createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
   name: "counter",
